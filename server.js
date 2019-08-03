03d1b492c322b7926e7937b13a7e4eac3d77ddea
vlad <vlad9rey@gmail.com>
2019-08-03 20:44:00 +0300

    initial express server with routes

--- server.js
+++ server.js
@@ -9,33 +9,36 @@ const posts = require("./routes/api/posts");
 
 const app = express();
 
-// Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// DB Config
-const db = require("./config/keys").mongoURI;
-
-// Connect to MongoDB
-mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
-
-// Passport middleware
-app.use(passport.initialize());
-
-// Passport Config
-require("./config/passport")(passport);
-
-// Use Routes
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.get("/", (req, res) => res.send("Hello"));
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log("0Server runing on port ${port}"));
+
+// // Body parser middleware
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// // DB Config
+const db = require("./config/keys").mongoURI;
+
+// // Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// // Passport middleware
+// app.use(passport.initialize());
+
+// // Passport Config
+// require("./config/passport")(passport);
+
+// // Use Routes
+// app.use("/api/users", users);
+// app.use("/api/profile", profile);
+// app.use("/api/posts", posts);
+
+// const port = process.env.PORT || 5000;
+
+// app.listen(port, () => console.log(`Server running on port ${port}`));

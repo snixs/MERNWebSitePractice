1e0a04b10f618b031c17b71f58a9b5136986efae
vlad <vlad9rey@gmail.com>
2019-08-09 23:41:53 +0300

    add profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -15,7 +15,6 @@ router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
 // @route   GET api/profile/handle/:handle
 // @desc    Get profile by handle
 // @access  Public
-
 router.get("/handle/:handle", (req, res) => {
   const errors = {};
 
@@ -32,27 +31,26 @@ router.get("/handle/:handle", (req, res) => {
     .catch(err => res.status(404).json(err));
 });
 
-// // @route   GET api/profile/user/:user_id
-// // @desc    Get profile by user ID
-// // @access  Public
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
 
-// router.get("/user/:user_id", (req, res) => {
-//   const errors = {};
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
 
-//   Profile.findOne({ user: req.params.user_id })
-//     .populate("user", ["name", "avatar"])
-//     .then(profile => {
-//       if (!profile) {
-//         errors.noprofile = "There is no profile for this user";
-//         res.status(404).json(errors);
-//       }
-
-//       res.json(profile);
-//     })
-//     .catch(err =>
-//       res.status(404).json({ profile: "There is no profile for this user" })
-//     );
-// });
+      res.json(profile);
+    })
+    .catch(err =>
+      res.status(404).json({ profile: "There is no profile for this user" })
+    );
+});
 
 // @route   GET api/profile/all
 // @desc    Get all profiles
@@ -73,7 +71,7 @@ router.get("/all", (req, res) => {
     .catch(err => res.status(404).json({ profile: "There are no profiles" }));
 });
 
-// @route   GET api/profile/all
+// @route   GET api/profile
 // @desc    Get profile by verification
 // @access  Private
 router.get(
@@ -94,57 +92,57 @@ router.get(
   }
 );
 
-// router.post(
-//   "/",
-//   passport.authenticate("jwt", { session: false }),
-//   (req, res) => {
-//     const { errors, isValid } = validateProfileInput(req.body);
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
 
-//     // Check Validation
-//     if (!isValid) {
-//       return res.status(400).json(errors);
-//     }
-//     const profilefields = {};
-//     profilefields.user = req.user.id;
-//     if (req.body.handle) profilefields.handle = req.body.handle;
-//     if (req.body.company) profilefields.company = req.body.company;
-//     if (req.body.website) profilefields.website = req.body.website;
-//     if (req.body.location) profilefields.location = req.body.location;
-//     if (req.body.bio) profilefields.bio = req.body.bio;
-//     if (req.body.status) profilefields.status = req.body.status;
-//     if (req.body.githubuser) profilefields.githubuser = req.body.githubuser;
+    // Check Validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    const profilefields = {};
+    profilefields.user = req.user.id;
+    if (req.body.handle) profilefields.handle = req.body.handle;
+    if (req.body.company) profilefields.company = req.body.company;
+    if (req.body.website) profilefields.website = req.body.website;
+    if (req.body.location) profilefields.location = req.body.location;
+    if (req.body.bio) profilefields.bio = req.body.bio;
+    if (req.body.status) profilefields.status = req.body.status;
+    if (req.body.githubuser) profilefields.githubuser = req.body.githubuser;
 
-//     if (typeof req.body.skills !== "undefined") {
-//       profilefields.skills = req.body.skills.split(",");
-//     }
+    if (typeof req.body.skills !== "undefined") {
+      profilefields.skills = req.body.skills.split(",");
+    }
 
-//     profilefields.social = {};
+    profilefields.social = {};
 
-//     if (req.body.youtube) profilefields.social.youtube = req.body.youtube;
-//     if (req.body.twitter) profilefields.social.twitter = req.body.twitter;
-//     if (req.body.facebook) profilefields.social.facebook = req.body.facebook;
-//     if (req.body.linkdin) profilefields.social.linkdin = req.body.linkdin;
-//     if (req.body.instagram) profilefields.social.instagram = req.body.instagram;
+    if (req.body.youtube) profilefields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profilefields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profilefields.social.facebook = req.body.facebook;
+    if (req.body.linkdin) profilefields.social.linkdin = req.body.linkdin;
+    if (req.body.instagram) profilefields.social.instagram = req.body.instagram;
 
-//     Profile.findOne({ user: req.user.id }).then(profile => {
-//       if (profile) {
-//         Profile.findOneAndUpdate(
-//           { user: req.user.id },
-//           { $set: profilefields },
-//           { new: true }
-//         ).then(profile => res.json(profile));
-//       } else {
-//         Profile.findOne({ hanlde: profilefields.handle }).then(profile => {
-//           if (profile) {
-//             errors.handle = "That handle already exists";
-//             res.status(400).json(errors);
-//           }
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profilefields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        Profile.findOne({ hanlde: profilefields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = "That handle already exists";
+            res.status(400).json(errors);
+          }
 
-//           new Profile(profilefields).save().then(profile => res.json(profile));
-//         });
-//       }
-//     });
-//   }
-// );
+          new Profile(profilefields).save().then(profile => res.json(profile));
+        });
+      }
+    });
+  }
+);
 
 module.exports = router;
